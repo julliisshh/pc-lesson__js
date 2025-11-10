1e66cf38fe6bb4e57056ea6223a28310c7c6c4ba
Julia <102296421+julliisshh@users.noreply.github.com>
2025-11-10 19:28:30 +0700

    2ой урок

---
+++ pc-lesson-main/js/smoothscroll.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector('.header__nav')
+const links = navbar.querySelectorAll('a')
+
+links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault()
+        console.log(seamless);
+        const section = document.querySelector(link.getAttribute('href'))
+
+        if (section) {
+            //section.scrollIntoView({
+             //   block: 'start',
+             //   behavior: 'smooth'
+            //})
+            seamless.scrollIntoView(section, {
+                behavior: "smooth",
+                block: "start",
+                inline: "start",
+            });
+        }
+    })
+})
